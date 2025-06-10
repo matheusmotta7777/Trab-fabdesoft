d6f2ce1e009f3f8f5f0b6ff3d411e305a7dcb9bd
matheusmotta7777 <matheus.motta@univille.br>
2025-06-10 00:55:30 +0000

    aula0906

--- projfabsoft-frontend/src/app/cliente/cliente.component.ts
+++ projfabsoft-frontend/src/app/cliente/cliente.component.ts
@@ -3,10 +3,11 @@ import { Cliente } from '../model/cliente';
 import { ClienteService } from '../service/cliente.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cliente',
-  imports: [HttpClientModule, CommonModule],
+  imports: [HttpClientModule, CommonModule, RouterLink],
   templateUrl: './cliente.component.html',
   styleUrl: './cliente.component.css',
   providers: [ClienteService]
@@ -14,7 +15,7 @@ import { CommonModule } from '@angular/common';
 export class ClienteComponent {
   listaClientes: Cliente[] = [];
 
-  constructor(private clienteService: ClienteService)
+  constructor(private clienteService: ClienteService){}
 
   ngOnInit(){
     console.log("Carregando clientes...");
